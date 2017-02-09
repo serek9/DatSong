0af785a0db89f972c135a2441463fd3afabba765
DAM <sergiodiaz808@gmail.com>
2017-02-09 21:08:19 +0100

    Initial Commit...

--- DatSong/FirstViewController.swift
+++ DatSong/FirstViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    let list = ["Tame Impala", "The Black Angels", "Pond", "Melody's Echo Chamber"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return (list.count)
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if editingStyle == UITableViewCellEditingStyle.Delete{
+            //TODO -- 7:45 (https://www.youtube.com/watch?v=LrCqXmHenJY)
+            //Commit Prueba
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
